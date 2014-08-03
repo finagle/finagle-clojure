d989eca11ef683787639d6206f3e4a620e9921b7
Sam Neubardt <samneubardt@gmail.com>
2014-08-03 12:54:09 -0400

    Add thrift as lein-sub module, remove codox
    codox isn't updating properly, not picking up changes
    to submodules. going to remove until i figure out how to fix

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
 (defproject finagle-clojure "0.1.0"
   :description "A light wrapper around Finagle for Clojure"
   :url "https://github.com/crashlytics/finagle-clojure"
-  :dependencies [[finagle-clojure/core "0.1.0"]]
+  :dependencies [[finagle-clojure/core "0.1.0"]
+                [finagle-clojure/thrift "0.1.0"]]
   :plugins [[lein-sub "0.3.0"]
             [codox "0.8.10"]
             [lein-midje "3.1.1"]]
-  :sub ["core"]
+  :sub ["core" "thrift"]
   :codox {:sources ["core/src"]
           :defaults {:doc/format :markdown}
           :output-dir "doc/codox"
