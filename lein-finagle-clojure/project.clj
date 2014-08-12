d0014ec668be8f5c00007dbf4d896a63fca4d6b1
Sam Neubardt <samneubardt@gmail.com>
2014-08-12 19:15:01 -0400

    Add scrooge linter task
    broken until scrooge supports not requiring a scala
    namespace definition. Projects that are only compiling
    Thrift to Java shouldn't need to define a Scala namespace.

--- lein-finagle-clojure/project.clj
+++ lein-finagle-clojure/project.clj
@@ -4,5 +4,6 @@
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :repositories [["sonatype" "https://oss.sonatype.org/content/groups/public/"]]
-  :dependencies [[com.twitter/scrooge-generator_2.10 "3.16.3"]]
+  :dependencies [[com.twitter/scrooge-generator_2.10 "3.16.3"]
+                 [com.twitter/scrooge-linter_2.10 "3.16.3"]]
   :eval-in-leiningen true)
--- lein-finagle-clojure/src/leiningen/finagle_clojure.clj
+++ lein-finagle-clojure/src/leiningen/finagle_clojure.clj
@@ -3,35 +3,43 @@
             [robert.hooke]
             [clojure.java.io :as io]))
 
+(defn thrift-files
+  [project-root source-path]
+  (->> source-path
+       (io/file project-root)
+       file-seq
+       (filter #(.isFile %))
+       (map #(.getPath %))))
+
 (defn scrooge
   "Compile Thrift definitions into Java classes using Scrooge.
 
-  Scrooge is a Thrift compiler that generates classes with 
+  Scrooge is a Thrift compiler that generates classes with
   Finagle appropriate interfaces (wraps Service method return values in Future).
-  
-  This task expects the following config to present on the project:
+
+  This task expects the following config to present on the project and will not run if it's absent:
 
     :finagle-clojure {:thrift-source-path \"\" :thrift-output-path \"\"}"
-  [project]
-   (let [project-root (:root project)
+  [project & options]
+   (let [subtask (first options)
+         project-root (:root project)
          source-path (get-in project [:finagle-clojure :thrift-source-path])
          raw-dest-path (get-in project [:finagle-clojure :thrift-output-path])]
      (if (and source-path raw-dest-path)
        (let [absolute-dest-path (->> raw-dest-path (io/file project-root) (.getAbsolutePath))
-             thrift-files (->> source-path
-                               (io/file project-root)
-                               file-seq 
-                               (filter #(.isFile %))
-                               (map #(.getPath %)))
+             thrift-files (thrift-files project-root source-path)
              scrooge-args (concat ["--finagle" "--skip-unchanged" "--language" "java" "--dest" absolute-dest-path] thrift-files)]
-         (leiningen.core.main/debug "Compiling thrift files:" thrift-files)
+         (prn subtask options)
+         (when (= subtask ":lint")
+           (leiningen.core.main/info "Linting Thrift files:" thrift-files)
+           (com.twitter.scrooge.linter.Main/main (into-array thrift-files)))
+         (leiningen.core.main/info "Compiling Thrift files:" thrift-files)
          (leiningen.core.main/debug "Calling scrooge with parameters:" scrooge-args)
          (com.twitter.scrooge.Main/main (into-array scrooge-args)))
        (leiningen.core.main/info "No config found for lein-finagle-clojure, not compiling Thrift for" (:name project)))))
 
 (defn javac-hook
   [f project & args]
-  (leiningen.core.main/info "Compiling Thrift")
   (scrooge project)
   (apply f project args))
 
@@ -41,7 +49,7 @@
    :subtasks [#'scrooge]}
   [project subtask & args]
   (case subtask
-    "scrooge" (scrooge project)))
+    "scrooge" (apply scrooge project args)))
 
 (defn activate []
   (robert.hooke/add-hook #'leiningen.javac/javac
