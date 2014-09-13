38453c876fc2af191b986e515325d61859b90f2a
Sam Neubardt <samneubardt@gmail.com>
2014-09-13 17:23:18 -0400

    Add scm info to lein-finagle-clojure/project.clj

--- lein-finagle-clojure/project.clj
+++ lein-finagle-clojure/project.clj
@@ -3,6 +3,8 @@
   :url "https://github.com/twitter/finagle-clojure"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
+   :scm {:name "git"
+         :url "http://github.com/finagle/finagle-clojure"}
   :repositories [["sonatype" "https://oss.sonatype.org/content/groups/public/"]]
   :dependencies [[com.twitter/scrooge-generator_2.10 "3.16.3"]]
   :eval-in-leiningen true)
