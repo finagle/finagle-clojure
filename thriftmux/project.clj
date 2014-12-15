6e456f7d4a79a264ed01369a3cd587b05dc75802
Sam Neubardt <samneubardt@gmail.com>
2014-12-14 20:59:34 -0500

    thriftmux depends on thrift to know how to import thrift classes
    at some point it may be worth deduplicating some more stuff between
    thriftmux & thrift. for the moment i think it's ok as it makes it a
    little easier to use thriftmux.
    
    it might make sense conceptually for a thriftmux server to `serve`
    a thrift `service` but that makes the API a little more complicated?

--- thriftmux/project.clj
+++ thriftmux/project.clj
@@ -14,12 +14,6 @@
   :java-source-paths ["test/java"]
   :jar-exclusions [#"test"]
   :test-paths ["test/clj/"]
-  ;; TODO there's no checksum for libthrift-0.5.0.pom, set checksum to warn for now
-  :repositories [["twitter" {:url "http://maven.twttr.com/" :checksum :warn}]]
-  ;; the dependency on finagle-clojure/core is required for tests
-  ;; but also to require fewer dependencies in projects that use thrift.
-  ;; this is akin to Finagle itself, where depending on finagle-thriftmux
-  ;; pulls in finagle-core as well.
-  :dependencies [[finagle-clojure/core "0.1.2-SNAPSHOT"]
-                 [com.twitter/finagle-thriftmux_2.10 "6.24.0"]
-                 [org.apache.thrift/libthrift "0.5.0-1"]])
+  :repositories [["twitter" {:url "http://maven.twttr.com/"}]]
+  :dependencies [[finagle-clojure/thrift "0.1.2-SNAPSHOT"]
+                 [com.twitter/finagle-thriftmux_2.10 "6.24.0"]])
