dd2c7bca3ec607c14ac93f51d6d8ef0b65f5fdcf
Brian Guthrie <brian.guthrie@soundcloud.com>
2014-12-24 17:07:24 -0500

    Dramatically improve type coercion, support more methods of querying.

--- mysql/src/finagle_clojure/mysql/client.clj
+++ mysql/src/finagle_clojure/mysql/client.clj
@@ -1,23 +1,44 @@
 (ns finagle-clojure.mysql.client
   (:import (com.twitter.finagle Stack$Param)
            (com.twitter.finagle.exp.mysql Row Value ByteValue ShortValue IntValue LongValue DoubleValue FloatValue
-                                          StringValue DateValue Type RawValue BigDecimalValue NullValue EmptyValue
-                                          NullValue$ EmptyValue$ Client PreparedStatement Result OK ResultSet Field)
+                                          StringValue Type RawValue BigDecimalValue NullValue EmptyValue
+                                          NullValue$ EmptyValue$ Client PreparedStatement Result OK ResultSet Field
+                                          DateValue$ TimestampValue)
            (com.twitter.finagle.exp Mysql$Client Mysql)
            (com.twitter.util Future)
-           (scala Function1))
+           (scala Function1)
+           (java.util TimeZone))
   (:require [finagle-clojure.scala :as scala]
             [finagle-clojure.options :as opt]))
 
 (defmulti unbox-raw-value
-  (fn [^RawValue v] (.typ v)))
+  (fn [^RawValue val] (.typ val)))
 
-(defmethod unbox-raw-value (Type/Date) [^RawValue val]
-  (-> val (DateValue/unapply) (opt/get)))
+(def utc-zone
+  (TimeZone/getTimeZone "UTC"))
+
+;; Punting on arbitrary timezone conversions for now, as UTC<->UTC is almost always the right thing to do.
+(def utc-timestamp-value
+  (TimestampValue. utc-zone utc-zone))
 
 (defmethod unbox-raw-value (Type/NewDecimal) [^RawValue val]
   (when-let [^scala.math.BigDecimal bd (-> val (BigDecimalValue/unapply) (opt/get))]
     (.underlying bd)))
+
+(defmethod unbox-raw-value (Type/Date) [^RawValue val]
+  (-> (.unapply DateValue$/MODULE$ val) (opt/get)))
+
+(defmethod unbox-raw-value (Type/Time) [^RawValue val]
+  (-> (.unapply utc-timestamp-value val) (opt/get)))
+
+(defmethod unbox-raw-value (Type/DateTime) [^RawValue val]
+  (-> (.unapply utc-timestamp-value val) (opt/get)))
+
+(defmethod unbox-raw-value (Type/Timestamp) [^RawValue val]
+  (-> (.unapply utc-timestamp-value val) (opt/get)))
+
+(defmethod unbox-raw-value :default [o]
+  (throw (RuntimeException. (str "Don't know how to unbox value: " o))))
 
 (defmulti unbox-value class)
 
@@ -30,8 +51,10 @@
 (defmethod unbox-value StringValue [^StringValue val] (-> val (.s)))
 (defmethod unbox-value NullValue$  [^NullValue _]     nil)
 (defmethod unbox-value EmptyValue$ [^EmptyValue _]    nil)
-(defmethod unbox-value RawValue    [^RawValue val]    (unbox-raw-value val))
-(defmethod unbox-value Value       [^Value val]       (throw (RuntimeException. (str "Unknown value type: " val))))
+(defmethod unbox-value RawValue    [^RawValue val]
+  (unbox-raw-value val))
+(defmethod unbox-value Value       [^Value val]
+  (throw (RuntimeException. (str "Don't know how to unbox value: " val))))
 
 (defmulti box-value class)
 
@@ -43,19 +66,48 @@
 (defmethod box-value Double         [d] (DoubleValue. d))
 (defmethod box-value String         [s] (StringValue. s))
 (defmethod box-value nil            [_] NullValue$/MODULE$)
-(defmethod box-value BigDecimal     [d] (BigDecimalValue/apply (scala.math.BigDecimal. d)))
-(defmethod box-value java.util.Date [d] (box-value (java.sql.Date. (.getTime d))))
-(defmethod box-value java.sql.Date  [d] (DateValue/apply d))
+(defmethod box-value BigDecimal     [d]
+  (BigDecimalValue/apply (scala.math.BigDecimal. d)))
+(defmethod box-value java.util.Date [d]
+  (box-value (java.sql.Date. (.getTime d))))
+(defmethod box-value java.sql.Date  [d]
+  (.apply DateValue$/MODULE$ d))
+(defmethod box-value java.sql.Timestamp [t]
+  (.apply utc-timestamp-value t))
+(defmethod box-value :default [val]
+  (throw (RuntimeException. (str "Don't know how to box value: " val))))
 
 (defn- Field->keyword [^Field f]
   (-> f (.name) (keyword)))
 
-(defn- Row->map [^Row row]
+(defn Row->map
+  "Given a Finagle MySQL row structure, convert it to a Clojure map of field names to idiomatic
+  Java data structures. Raises an exception when it can't find an appropriate way to convert
+  the boxed MySQL value.
+
+  *Arguments:*
+
+    * `row`: a [[com.twitter.finagle.exp.mysql.Row]]
+
+  *Returns:*
+
+    a Clojure map of field/value pairs"
+  [^Row row]
   (zipmap
     (map Field->keyword  (scala/scala-seq->vec (.fields row)))
     (map unbox-value (scala/scala-seq->vec (.values row)))))
 
 (defn ResultSet->vec [^ResultSet rs]
+  "Given a Finagle MySQL result set, convert it to a vector of Clojure maps using `Row->map`. Raises
+  an exception when it can't find an appropriate way to convert the boxed MySQL value.
+
+  *Arguments:*
+
+    * `rs`: a [[com.twitter.finagle.exp.mysql.ResultSet]]
+
+  *Returns:*
+
+    a Clojure vector of maps"
   (->> (.rows rs)
        (scala/scala-seq->vec)
        (map Row->map)))
@@ -172,16 +224,43 @@
   [^Client client sql]
   (.query client sql))
 
-;; Future[Seq[T]]
-(defmulti ^Future select class)
+(defn- ^com.twitter.util.Function wrap-fn [f]
+  (if-not (instance? scala.Function f)
+    (scala/Function* f)
+    f))
 
-(defmethod select PreparedStatement
-  [^PreparedStatement stmt params ^Function1 fn1]
-  (.select stmt (scala/seq->scala-buffer (map box-value params)) fn1))
+(defn ^Future select
+  "Given a rich client, a SQL string, and a mapping function, executes the SQL and returns the result as a
+  Future[Seq[T]], where T is the type yielded by the given mapping function.
 
-(defmethod select Client
+
+  *Arguments:*
+
+    * `client`: a rich MySQL `Client`
+    * `sql`: a SQL string
+    * `fn1`: a Clojure or Scala Function1 that accepts a [[com.twitter.finagle.exp.mysql.Row]] and returns an arbitrary type
+
+  *Returns:*
+
+    a `Future` containing a Scala Seq whose contents are derived from the given function"
   [^Client client sql ^Function1 fn1]
-  (.select client sql fn1))
+  (.select client sql (wrap-fn fn1)))
+
+(defn ^Future select-stmt
+  "Given a `PreparedStatement`, a vector of params, and a mapping function, executes the parameterized statement
+  and returns the result as a `Future[Seq[T]]`, where T is the type yielded by the given mapping function.
+
+  *Arguments*:
+
+    * `stmt`: a `PreparedStatement`, generally derived from the `prepare` function
+    * `params`: a Clojure vector of params
+    * `fn1`: a Clojure or Scala Function1 that accepts a [[com.twitter.finagle.exp.mysql.Row]] and returns an arbitrary type
+
+  *Returns:*
+
+    a `Future` containing a Scala Seq whose contents are derived from the given function"
+  [^PreparedStatement stmt params ^Function1 fn1]
+  (.select stmt (scala/seq->scala-buffer (map box-value params)) (wrap-fn fn1)))
 
 (defn ^PreparedStatement prepare
   "Given a rich client and a SQL string, returns a `PreparedStatement` ready to be parameterized and executed.
@@ -241,5 +320,15 @@
   [^Result result]
   (or (instance? OK result) (instance? ResultSet result)))
 
-(defn affected-rows [^Result result]
+(defn affected-rows
+  "Given a `Result`, returns the number of rows affected (deleted, inserted, created) by that query.
+
+   *Arguments:*
+
+     * `result`: a [[com.twitter.finagle.exp.mysql.Result]]
+
+   *Returns:*
+
+     the number of rows affected by the query that generated the given `Result`"
+  [^Result result]
   (.affectedRows result))
--- mysql/test/finagle_clojure/mysql/integration_test.clj
+++ mysql/test/finagle_clojure/mysql/integration_test.clj
@@ -1,12 +1,15 @@
 (ns finagle-clojure.mysql.integration-test
   (:import (com.twitter.finagle.exp.mysql BigDecimalValue)
-           (com.twitter.util Future))
+           (com.twitter.util Future)
+           (java.sql Date Timestamp)
+           (java.util TimeZone))
   (:require [midje.sweet :refer :all]
             [finagle-clojure.mysql.client :refer :all]
             [finagle-clojure.futures :as f]
             [finagle-clojure.scala :as scala]))
 
 (System/setProperty "java.net.preferIPv4Stack" "true")
+(TimeZone/setDefault (TimeZone/getTimeZone "UTC"))
 
 (defn mapfn [^Future fut func]
   (f/map* fut (scala/Function* func)))
@@ -23,36 +26,107 @@
           (f/await))
       => true)
 
-    (fact "it can create and query a table"
+    (fact "it can create and update a table"
 
       (-> (query db "CREATE TABLE widgets (
                        id INT AUTO_INCREMENT PRIMARY KEY,
+                       sprockets SMALLINT,
                        name VARCHAR(255),
                        description TEXT,
                        price DECIMAL(10,2),
-                       created_at DATETIME
+                       mfd_on DATE,
+                       created_at TIMESTAMP
                      )")
           (mapfn ok?)
           (f/await))
       => true
 
-      (-> (prepare db "INSERT INTO widgets (name, description, price)
-                       VALUES (?, ?, ?)")
-          (exec "fizzbuzz" "a fizzy buzzy" 10.2M)
+      (-> (prepare db "INSERT INTO widgets (name, description, sprockets, price, mfd_on, created_at)
+                       VALUES (?, ?, ?, ?, ?, ?)")
+          (exec
+            "fizzbuzz"
+            "a fizzy buzzy"
+            (short 12)
+            10.2M
+            (Date/valueOf "2014-12-23")
+            (Timestamp/valueOf "2014-12-24 10:11:12")
+            )
           (mapfn affected-rows)
           (f/await))
-      => 1
+      => 1)
 
-      (-> (prepare db "SELECT * FROM WIDGETS")
-          (exec nil)
-          (mapfn ResultSet->vec)
-          (f/await))
-      => [{:id 1 :name "fizzbuzz" :description "a fizzy buzzy" :price 10.2M :created_at nil}]
+    (fact "it understands how to unbox a wide range of data types"
+      (let [rows (-> (prepare db "SELECT * FROM WIDGETS")
+                     (exec)
+                     (mapfn ResultSet->vec)
+                     (f/await))]
+        (count rows)
+        => 1
 
+        (-> rows (first) (get :id))
+        => 1
+
+        (-> rows (first) (get :name))
+        => "fizzbuzz"
+
+        (-> rows (first) (get :description))
+        => "a fizzy buzzy"
+
+        (-> rows (first) (get :price))
+        => 10.2M
+
+        (-> rows (first) (get :sprockets))
+        => 12
+
+        ;; TODO  Once bug fix is applied, assert with Date/valueOf rather than constituent parts.
+        ;; (-> rows (first) (get :mfd_on))
+        ;; => (Date/valueOf "2014-12-23")
+
+        ;; FIXME There appears to be a bug in com.twitter.finagle.exp.mysql.DateValue.fromBytes that fails to
+        ;;       zero out time values generated when getting a Calendar instance, leading to a Date object with
+        ;;       erroneous time information. This must be fixed upstream in finagle-mysql.
+        (-> rows (first) (get :mfd_on) (.getYear) (+ 1900)) ;; Years are post-1900
+        => 2014
+
+        (-> rows (first) (get :mfd_on) (.getMonth) (+ 1)) ;; Months are zero-indexed
+        => 12
+
+        (-> rows (first) (get :mfd_on) (.getDate))
+        => 23
+
+        (-> rows (first) (get :created_at))
+        => (Timestamp/valueOf "2014-12-24 10:11:12")
+        ))
+
+    (fact "it can select from the table using the rich client"
+      (let [rows (-> (select db "SELECT * FROM widgets" Row->map)
+                     (mapfn scala/scala-seq->vec)
+                     (f/await))]
+        (count rows)
+        => 1
+
+        (-> rows (first) (select-keys [:id :name]))
+        => {:id 1 :name "fizzbuzz"}
+        ))
+
+    (fact "it can select from the table using a prepared statement"
+      (let [rows (-> (prepare db "SELECT * FROM widgets")
+                     (select-stmt [] Row->map)
+                     (mapfn scala/scala-seq->vec)
+                     (f/await))]
+        (count rows)
+        => 1
+
+        (-> rows (first) (select-keys [:id :name]))
+        => {:id 1 :name "fizzbuzz"}
+        ))
+
+    (fact "it can clean up after the tests by deleting a table"
       (-> (query db "DROP TABLE widgets")
           (mapfn ok?)
           (f/await))
-      => true
-    ))
+      => true)
+
+    )
 
   (-> (mysql-client) (with-credentials "root" "") (with-database "finagle_clojure_test") (rich-client "localhost:3306")))
