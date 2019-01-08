dc33ac6b41c5fd41113575ae83b28e1d79f21eda
patrickds <patrickds@outlook.com>
2019-01-08 11:30:27 -0200

    Configure release repo

--- http/project.clj
+++ http/project.clj
@@ -4,11 +4,14 @@
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :scm {:name "git" :url "https://github.com/finagle/finagle-clojure"}
-  :plugins [[lein-midje "3.2"]]
+  :plugins [[s3-wagon-private "1.3.1"]
+            [lein-midje "3.2"]]
   :profiles {:test {:dependencies [[midje "1.8.3" :exclusions [org.clojure/clojure]]]}
              :dev [:test {:dependencies [[org.clojure/clojure "1.8.0"]]}]
              :1.7 [:test {:dependencies [[org.clojure/clojure "1.7.0"]]}]
              :1.6 [:test {:dependencies [[org.clojure/clojure "1.6.0"]]}]
              :1.5 [:test {:dependencies [[org.clojure/clojure "1.5.1"]]}]}
+  :repositories [["nu-maven" {:url "s3p://nu-maven/releases/"}]]
+  :deploy-repositories [["releases" {:url "s3p://nu-maven/releases/" :no-auth true}]]
   :dependencies [[finagle-clojure/core "0.7.1-NUBANK"]
                  [com.twitter/finagle-http_2.11 "18.7.0"]])
--- thrift/project.clj
+++ thrift/project.clj
@@ -4,11 +4,12 @@
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :scm {:name "git" :url "https://github.com/finagle/finagle-clojure"}
-  :plugins [[lein-midje "3.2"]
-            [lein-finagle-clojure "0.7.1-NUBANK" :hooks false]]
+  :plugins [[s3-wagon-private "1.3.1"]
+            [lein-midje "3.2"]]
   :profiles {:test {:dependencies [[midje "1.8.3" :exclusions [org.clojure/clojure]]]
                     :resource-paths ["test/resources"]}
-             :dev [:test {:dependencies [[org.clojure/clojure "1.8.0"]]}]
+             :dev {:test {:dependencies [[org.clojure/clojure "1.8.0"]]}
+                   :plugins [[lein-finagle-clojure "0.7.1-NUBANK"]]}
              :1.7 [:test {:dependencies [[org.clojure/clojure "1.7.0"]]}]
              :1.6 [:test {:dependencies [[org.clojure/clojure "1.6.0"]]}]
              :1.5 [:test {:dependencies [[org.clojure/clojure "1.5.1"]]}]}
@@ -17,7 +18,9 @@
   :jar-exclusions [#"test"]
   :test-paths ["test/clj/"]
   ;; TODO there's no checksum for libthrift-0.5.0.pom, set checksum to warn for now
-  :repositories [["twitter" {:url "https://maven.twttr.com/" :checksum :warn}]]
+  :repositories [["nu-maven" {:url "s3p://nu-maven/releases/"}]
+                 ["twitter" {:url "https://maven.twttr.com/" :checksum :warn}]]
+  :deploy-repositories [["releases" {:url "s3p://nu-maven/releases/" :no-auth true}]]
   ;; the dependency on finagle-clojure/core is required for tests
   ;; but also to require fewer dependencies in projects that use thrift.
   ;; this is akin to Finagle itself, where depending on finagle-thrift
