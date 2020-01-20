1852ee1e6262f190e5eddeae07dd6f2ebdcf2932
Luiz Hespanha <luiz.hespanha@gmail.com>
2020-01-20 18:02:26 -0300

    Version 0.8.0

--- core/project.clj
+++ core/project.clj
@@ -1,4 +1,4 @@
-(defproject finagle-clojure/core "0.8.0-NUBANK"
+(defproject finagle-clojure/core "0.8.0"
   :description "A light wrapper around Finagle & Twitter Util for Clojure"
   :url "https://github.com/twitter/finagle-clojure"
   :license {:name "Apache License, Version 2.0"
